80ec3664622ddc58ad31a0d63c2bc5f25ce6d95f
Aleksanteri <aleksanteri.jaakola@edu.turkuamk.fi>
2021-03-13 17:55:32 +0200

    Redesigned website

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const container = document.querySelector(".container");
+// const hamburger_menu = document.querySelector(".hamburger-menu");
+// const container = document.querySelector(".container");
 
-hamburger_menu.addEventListener("click", () => {
-    container.classList.toggle("active")
-})
+// hamburger_menu.addEventListener("click", () => {
+//     container.classList.toggle("active")
+// })
